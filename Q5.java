bdc55425a0d4de9a586aca44ace8aac70ea9c540
DonovanLef <130224718+DonovanLef@users.noreply.github.com>
2024-11-20 14:23:33 +0100

    Create Q5.java

---
+++ Q5.java
@@ -0,0 +1,20 @@
+public class Q5 {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Veuillez fournir le nombre d'étages du triangle.");
+            return;  
+        }
+
+        try { 
+            int numRows = Integer.parseInt(args[0]); 
+            for (int i = 1; i <= numRows; i++) {
+                for (int j = 0; j < i; j++) { 
+                    System.out.print("*"); 
+                }
+                System.out.println();
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Le paramètre doit être un entier.");
+        }
+    }
+}
